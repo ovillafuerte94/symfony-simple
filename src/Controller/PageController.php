e431d91f0f7a28e81a36a0792d19fe0b38584357
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-14 15:22:38 -0400

    Add homepage

---
+++ src/Controller/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController
+{
+    #[Route('/')]
+    public function home(Request $request): Response
+    {
+        $search = $request->get('search');
+
+        return new Response('Welcome to home page! ' . $search);
+    }
+}
