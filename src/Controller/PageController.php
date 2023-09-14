55cbbcd0dd1b7dc7a147f1a2a7533532d3e9d4f9
Omar Villafuerte <ovillafuerte.94@gmail.com>
2023-09-14 18:17:01 -0400

    Create form and save comment in database

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -11,16 +13,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/')]
-    public function home(EntityManagerInterface $entityManager): Response
+    #[Route('/', name: 'home')]
+    public function home(EntityManagerInterface $entityManager, Request $request): Response
     {
-        $comments = $entityManager->getRepository(Comment::class)->findBy([], [
-            'id' => 'DESC'
-        ]);
+        $form = $this->createForm(CommentType::class);
+        $form->handleRequest($request);
+
+        if ( $form->isSubmitted() ) {
+            $entityManager->persist($form->getData());
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
+
+        $comments = $entityManager->getRepository(Comment::class)->findBy([], ['id' => 'DESC']);
         
         return $this->render('home.html.twig', [
-            // 'comments' => $entityManager->getRepository(Comment::class)->findAll()
-            'comments' => $comments
+            'comments' => $comments,
+            'form' => $form->createView()
         ]);
     }
 }
